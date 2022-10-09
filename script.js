2308a69c57fee82b32366c05213761a383fc5923
Damien Pinna <damien.pinna@gmail.com>
2022-10-09 12:42:30 +0200

    ajout effet fade-in sur la description

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const imagesCanettes = document.querySelectorAll(".slider-img-canettes img");
+const description = document.querySelectorAll(".item-description");
 const nbImagesCannettes = imagesCanettes.length;
 const next = document.querySelector(".next");
 let count = 0;
 
 const canetteSuivante = () => {
   imagesCanettes[count].classList.remove("active");
+  description[count].classList.remove("active");
 
   if (count < nbImagesCannettes - 1) {
     count++;
@@ -13,6 +15,7 @@ const canetteSuivante = () => {
   }
 
   imagesCanettes[count].classList.add("active");
+  description[count].classList.add("active");
 };
 
 next.addEventListener("click", canetteSuivante);
